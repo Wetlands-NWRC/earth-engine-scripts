bfa1e3fee872f215dfafcc6d1fcc14b5e5d63579
Ryan Hamilton <ryangilberthamilton@gmail.com>
2023-08-15 15:33:25 -0400

    update name and version

--- s1-tile-extractor/setup.py
+++ s1-tile-extractor/setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup, find_packages
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     setup(
-        name='s1_data_extractor',
-        version='0.0.1',
+        name="s1_tile_extractor",
+        version="1.0.0",
         install_requires=[
             "earthengine-api",
             "geopandas",
         ],
         packages=find_packages(),
         entry_points={
-            'console_scripts': [
-                's1datex = app:main',
+            "console_scripts": [
+                "s1tilex = app:main",
             ]
-        }
+        },
     )
